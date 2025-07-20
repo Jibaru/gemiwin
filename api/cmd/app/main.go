252c996049e4c0c9e0058650c45743864a5df489
Jibaru <ignacioruedaboada@hotmail.com>
2025-07-20 09:46:42 -0500

    feat: add port flag

--- api/cmd/app/main.go
+++ api/cmd/app/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+
 	"gemiwin/api/server"
 )
 
 func main() {
+	// Define the `-port` flag (default 8080)
+	port := flag.String("port", "8080", "Port for the HTTP server")
+	flag.Parse()
+
+	addr := fmt.Sprintf(":%s", *port)
+
 	s := server.New()
-	s.Run(":8080")
+	if err := s.Run(addr); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
